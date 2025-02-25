442ec78fb4f7a74ed83f3239bba16990e3eccc65
sdsadfaf <wyq977@users.noreply.github.com>
2025-02-25 19:09:17 +0100

    Add documentation for EValTwoProp

--- R/EValTwoProp.R
+++ R/EValTwoProp.R
@@ -1,39 +1,75 @@
-likelihoodBernoulli <- function(n, x, theta) {
-  # Use log and exp to avoid underflow/overflow
-  return(x * log(theta) + (n - x) * log(1 - theta))
-}
-
-
-# TODO: update priors with none restrictions in k x 2 settings
-# TODO: change Beta dist. parameters
-# TODO: default two-sided tests only
-#
-#' Calculate e-value for 2 x 2 contingency table
+#' Calculate the e-value for a Safe Experiment to Test Two Proportions in Stream Data
+#'
+#' @param ya positive observations/events per data block in group a: a numeric with integer values
+#' between (and including) 0 and \code{na}, the number of observations in group a per block.
+#' @param yb positive observations/events per data block in group b: a numeric with integer values
+#' between (and including) 0 and \code{nb}, the number of observations in group b per block.
+#' @param alternative comparision in the alternative hypothesis
+#' @param na number of observations in group a per data block
+#' @param nb number of observations in group b per data block.
+#' The number of data blocks arrived in two streams (group a/b) must be the same
+#' while the size of data block can vary (\code{na} != \code{nb}).
+#' @param esType a character string specifying an optional restriction on the alternative hypothesis; must be one of "none" (default),
+#' "difference" (difference group mean b minus group b) or "logOddsRatio" (the log odds ratio between group means b and a).
+#' @param esMin a priori minimal relevant divergence between group means b and a, either a numeric between -1 and 1 for
+#' no alternative restriction or a restriction on difference, or a real for a restriction on the log odds ratio.
+#' @param alpha numeric in (0, 1)
+#' @param prior a list of shape parameters for priori Beta distribution for group a, group b.
+#'
+#' @return e-value between 0 and \code{Inf}
 #' @export
 EValTwoProp <- function(ya, yb, alternative = c("twoSided", "greater", "less"),
-                        na = 1, nb = 1, esType = c("logOddsRatio", "difference", "none"),
+                        na = 1, nb = 1, esType = c("none", "logOddsRatio", "difference"),
                         esMin = NULL, alpha = 0.05, prior = NULL) {
+  # argument validation
+  alternative <- match.arg(alternative)
+  esType <- match.arg(esType)
 
+  if (length(ya) != length(yb)) stop("The numer of samples in group A and group B must be equal!")
+
+  if (max(ya) > na || min(ya) < 0 || max(yb) > nb || min(yb) < 0) {
+    stop(paste0(
+      "Number of observations cannot be negative or larger than the block size!\n",
+      "    Expected:  ya in [0, ", na, "]\t", "Actual: ya in [", min(ya), ", ", max(ya), "]\n",
+      "    Expected:  yb in [0, ", nb, "]\t", "Actual: yb in [", min(yb), ", ", max(yb), "]\n"
+    ))
+  }
+
+  if (esType == "none" && !is.null(esMin)) {
+    stop("There should be no minimum difference in this case!")
+  } else if (esType == "difference") {
+    if (!is.numeric(esMin)) {
+      stop("For absolute difference cases, esMin must be numeric.")
+    } else if (esMin < 0 || esMin > 1) {
+      stop("For absolute difference cases, esMin must be between 0 and 1.")
+    }
+  } else if (esType == "logOddsRatio") {
+    # esMin can be a float between -Inf and Inf
+    if (!is.finite(esMin)) {
+      stop("For logOddsRatio cases, esMin must be a finite value.\n
+           Consider setting esType == \"none\" as there is no restriction placed\n
+           on parameters when logOddsRatio == Inf or -Inf.")
+    }
+  }
+
+  # unpack the prior values
   if (is.null(prior)) {
     # print("Using the default parameters (alpha = beta = 0.18) for Beta distribution...")
     betaA1 <- betaA2 <- betaB1 <- betaB2 <- 0.18
   } else {
-    # unpack the prior values
     betaA1 <- prior[["betaA1"]]
     betaA2 <- prior[["betaA2"]]
     betaB1 <- prior[["betaB1"]]
     betaB2 <- prior[["betaB2"]]
   }
 
-  eVal <- 1
 
-  # case 1: No restriction on prior --------------------------------------------
+  # Case 1: No restriction on prior --------------------------------------------
   if (esType == "none") {
-    if (!is.null(esMin)) {
-      stop("There should be no minimum difference in this case!")
-    } else if (length(ya) != length(yb)) {
-      stop("The numer of samples in group A and group B must be equal!")
-    }
+    eVal <- 1
+
+    # naive approach
+    thetaA <- thetaB <- theta0 <- 0.5
 
     # init vector
     totalSuccessA <- cumsum(ya)
@@ -43,64 +79,30 @@ EValTwoProp <- function(ya, yb, alternative = c("twoSided", "greater", "less"),
     totalFailA <- groupSizeVecA - totalSuccessA
     totalFailB <- groupSizeVecB - totalSuccessB
 
-    # # naive approach
-    # thetaA <- thetaB <- theta0 <- 0.5
-    #
-    # for (i in seq_along(ya)) {
-    #   newE <- safestats:::calculateETwoProportions(
-    #     na1 = ya[i],
-    #     na = na,
-    #     nb1 = yb[i],
-    #     nb = nb,
-    #     thetaA = thetaA,
-    #     thetaB = thetaB,
-    #     theta0 = theta0
-    #   )
-    #
-    #   eVal <- eVal * newE
-    #
-    #   thetaA <- safestats:::bernoulliMLTwoProportions(totalSuccessA[i], totalFailA[i], betaA1, betaA2)
-    #
-    #   thetaB <- safestats:::bernoulliMLTwoProportions(totalSuccessB[i], totalFailB[i], betaB1, betaB2)
-    #
-    #   theta0 <- (na * thetaA + nb * thetaB) / (na + nb)
-    # }
+    for (i in seq_along(ya)) {
+      newE <- safestats:::calculateETwoProportions(
+        na1 = ya[i], na = na, nb1 = yb[i], nb = nb, thetaA = thetaA,
+        thetaB = thetaB, theta0 = theta0
+      )
 
-    # vectorized approach
-    thetaA <- thetaB <- theta0 <- rep(0.5, length(ya))
+      eVal <- eVal * newE
 
-    # vectorize operation except 1st
-    theta <- safestats:::updateETwoProportions(
-      totalSuccessA[-length(totalSuccessA)], totalFailA[-length(totalFailA)],
-      totalSuccessB[-length(totalSuccessB)], totalFailB[-length(totalFailB)],
-      na, nb,
-      betaA1, betaA2,
-      betaB1, betaB2
-    )
+      thetaA <- safestats:::bernoulliMLTwoProportions(totalSuccessA[i], totalFailA[i], betaA1, betaA2)
 
-    # assign values to all elements except 1st
-    thetaA[-1] <- theta$thetaA
-    thetaB[-1] <- theta$thetaB
-    theta0[-1] <- theta$theta0
+      thetaB <- safestats:::bernoulliMLTwoProportions(totalSuccessB[i], totalFailB[i], betaB1, betaB2)
 
-    eValVec <- safestats:::calculateETwoProportions(
-      na1 = ya, na = na, nb1 = yb, nb = nb,
-      thetaA = thetaA, thetaB = thetaB, theta0 = theta0
-    )
-
-    eVal <- prod(eValVec)
-
+      theta0 <- (na * thetaA + nb * thetaB) / (na + nb)
+    }
   } else if (esType == "difference" || esType == "logOddsRatio") {
-    # case 2/3: restrict difference on (0, 1)^2 para. space --------------------
+    # Case 2: Specified minimal effect difference (esMin) ----------------------
+    # on (0, 1)^2 para. space
     # they all used Bayensian updating in each timesteps between blocks
     # the only difference are the calculation of thetaB and grid
-    if (!is.numeric(esMin)) {
-      stop("esMin must be numeric for difference")
-    }
-    gridSize <- 1e3
-    K <- 1 / gridSize
 
-    rhoGrid <- seq(K, 1 - K, length.out = gridSize)
+    # avoid 0 and 1 to prevent numerical instability
+    gridSize <- 1e3
+
+    rhoGrid <- seq(1 / gridSize, 1 - 1 / gridSize, length.out = gridSize)
 
     ## difference in probability grid ------------------------------------------
     if (esType == "difference") {
@@ -109,8 +111,15 @@ EValTwoProp <- function(ya, yb, alternative = c("twoSided", "greater", "less"),
       thetaBgrid <- thetaAgrid + esMin
     } else if (esType == "logOddsRatio") {
       thetaAgrid <- rhoGrid
-      thetaBgrid <- sapply(thetaAgrid, calculateThetaBFromThetaAAndLOR, lOR = delta)
+      thetaBgrid <- safestats:::calculateThetaBFromThetaAAndLOR(thetaAgrid, esMin)
     }
+
+    # prepare log theta
+    logThetaAgrid <- log(thetaAgrid)
+    logThetaBgrid <- log(thetaBgrid)
+    logOneMinusThetaAgrid <- log(1 - thetaAgrid)
+    logOneMinusThetaBgrid <- log(1 - thetaBgrid)
+
 
     # prior density for thetaA at each grid points with normalization
     # rho = thetaA / (1 - esMin) ~ Beta(shape1, shape2)
@@ -118,12 +127,12 @@ EValTwoProp <- function(ya, yb, alternative = c("twoSided", "greater", "less"),
     thetaADensity <- thetaADensity / sum(thetaADensity)
 
     # TODO: Is the initial theta 1 / 2?
-    thetaB <- thetaA <- theta0 <- rep(0.5, length(ya))
+    thetaB <- thetaA <- theta0 <- numeric(length(ya))
 
-    for (i in 1:length(ya)) {
+    for (i in seq_along(ya)) {
       # Compute log-likelihoods for thetaA and thetaB
-      logLikelihoodA <- likelihoodBernoulli(na, ya[i], thetaAgrid)
-      logLikelihoodB <- likelihoodBernoulli(nb, yb[i], thetaBgrid)
+      logLikelihoodA <- ya[i] * logThetaAgrid + (na - ya[i]) * logOneMinusThetaAgrid
+      logLikelihoodB <- yb[i] * logThetaBgrid + (nb - yb[i]) * logOneMinusThetaBgrid
 
       # Compute posterior in log-space for stability
       logPosterior <- logLikelihoodA + logLikelihoodB + log(thetaADensity)
@@ -139,10 +148,11 @@ EValTwoProp <- function(ya, yb, alternative = c("twoSided", "greater", "less"),
     }
 
     ## difference in calculating thetaB ----------------------------------------
+    # TODO: add validation for esMin < 0
     if (esType == "difference") {
       thetaB <- thetaA + esMin
     } else if (esType == "logOddsRatio") {
-      thetaB <- safestats:::calculateThetaBFromThetaAAndLOR(thetaA = thetaA, lOR = esMin)
+      thetaB <- safestats:::calculateThetaBFromThetaAAndLOR(thetaA, esMin)
     }
 
     theta0 <- (na * thetaA + nb * thetaB) / (na + nb)
